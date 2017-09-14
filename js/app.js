fc86b4c8020b082f6716ba17889ee63678bde7ca
Harish247 <g.harish247@gmail.com>
2017-09-13 23:55:06 -0700

    handling refresh
    adding init function to handle click on refresh button

--- js/app.js
+++ js/app.js
@@ -21,10 +21,6 @@ let counter = 0;
 $('.moves').text(counter);
 let shuffledItems = shuffle(items);
 let deck = $('.deck');
-shuffledItems.forEach(function(element) {
-    deck.append(`<li class="card"><i class="fa fa-${element}"></i></li>`);
-});
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -39,24 +35,6 @@ function shuffle(array) {
 
     return array;
 }
-
-let cards = $('.card');
-cards.each(function(index){
-    this.addEventListener('click',function(e){
-        if($(this).hasClass('match')==false && $(this).hasClass('open show')==false){
-            countMoves();
-            displayCard($(this));
-            if(openCards.length>1){
-                if(openCards[0].children().attr("class") === openCards[1].children().attr("class")){
-                    freezCards();
-                }else{
-                    closeCards();
-                }
-            }
-        }
-    });
-});
-
 function displayCard(element){
     element.addClass('open show');
     openCards.push(element);
@@ -81,13 +59,33 @@ function countMoves(){
     counter++;
     $('.moves').text(counter);
 }
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+$('.restart').click(function(e){
+    init();
+});
+function init(){
+    openCards = [];
+    counter = 0;
+    $('.moves').text(counter);
+    $('.card').each(function(index){
+        $(this).remove();
+    });
+    shuffledItems.forEach(function(element) {
+        deck.append(`<li class="card"><i class="fa fa-${element}"></i></li>`);
+    });
+    $('.card').each(function(index){
+        this.addEventListener('click',function(e){
+            if($(this).hasClass('match')==false && $(this).hasClass('open show')==false){
+                countMoves();
+                displayCard($(this));
+                if(openCards.length>1){
+                    if(openCards[0].children().attr("class") === openCards[1].children().attr("class")){
+                        freezCards();
+                    }else{
+                        closeCards();
+                    }
+                }
+            }
+        });
+    });
+}
+init();
