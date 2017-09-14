b6c5b410ad233f05c95e4a9236e200f6f13c7852
Harish247 <g.harish247@gmail.com>
2017-09-14 01:38:18 -0700

    adjusting stars, displaying results
    added styling and counting for stars, added results page to show the results.

--- js/app.js
+++ js/app.js
@@ -21,6 +21,8 @@ let counter = 0;
 $('.moves').text(counter);
 let shuffledItems = shuffle(items);
 let deck = $('.deck');
+let stars = $('.fa.fa-star');
+let starCount = 3;
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -45,6 +47,9 @@ function freezCards(){
         element.addClass('match');   
     });
     openCards = [];
+    if($('.match').length == 16){
+        displayResult();
+    }
 }
 function closeCards(){
     openCards.forEach(function(element) {
@@ -62,9 +67,36 @@ function countMoves(){
 $('.restart').click(function(e){
     init();
 });
+function adjustStars(){
+    if(counter<25){
+        starcount = 3;
+    }else if(counter<35){
+        $('#star3').css({'color':'#2B2727'});
+        starCount = 2;
+    }else{
+        $('#star2').css({'color':'#2B2727'});
+        starCount = 1;
+    }
+}
+function displayResult(){
+    $('.container').hide();
+    let result = $('<div id="result-container" align="center"></div>');
+    $('body').append(result);
+    $('#result-container').append('<h3>Congratulations! You have won!</h3>');
+    $('#result-container').append(`with ${counter} moves and ${starCount} stars.</br>`);
+    $('#result-container').append('<input type="button" id="play-again" value="Play again!">');
+    $('#play-again').click(function(e){
+        $('#result-container').remove();
+        $('.container').show();
+        init();
+    });
+}
 function init(){
     openCards = [];
     counter = 0;
+    $('#star1').css({'color':'#CEA120'})
+    $('#star2').css({'color':'#CEA120'})
+    $('#star3').css({'color':'#CEA120'})
     $('.moves').text(counter);
     $('.card').each(function(index){
         $(this).remove();
@@ -76,6 +108,7 @@ function init(){
         this.addEventListener('click',function(e){
             if($(this).hasClass('match')==false && $(this).hasClass('open show')==false){
                 countMoves();
+                adjustStars();
                 displayCard($(this));
                 if(openCards.length>1){
                     if(openCards[0].children().attr("class") === openCards[1].children().attr("class")){
